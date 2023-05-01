9e8947bfaf94a99851d21609f148546601eda995
Walter <walterxie@users.noreply.github.com>
2023-05-01 13:40:15 +1200

    more in GraphicalModel

--- lphy/core/graphicalmodel/GraphicalModel.py
+++ lphy/core/graphicalmodel/GraphicalModel.py
@@ -17,3 +17,18 @@ class GraphicalModel:
 
     def getModelValues(self) -> set:
         pass
+
+    def getValue(self, __id, context):
+        match context:
+            case "data":
+                return self.getDataDictionary().get(__id)
+            case "model" | _:
+                _data = self.getDataDictionary()
+                _model = self.getModelDictionary()
+                if __id in _model:
+                    return _model.get(__id)
+                return _data.get(__id)
+
+    def hasValue(self, __id, context):
+        return bool(self.getValue(__id, context))
+
