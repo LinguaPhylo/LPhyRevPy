eb1eaafa59ea6a098a5bed9e07698751d4784226
kche309 <8745224+kche309@users.noreply.github.com>
2023-05-01 12:18:54 +1200

    SimulatorBaseListener revert method names

--- lphy/core/parser/SimulatorBaseListener.py
+++ lphy/core/parser/SimulatorBaseListener.py
@@ -1,12 +1,10 @@
 # Generated from LPhyRevPy/lphy/core/parser/Simulator.g4 by ANTLR 4.12.0
 
 from antlr4 import *
-
 if __name__ is not None and "." in __name__:
     from .SimulatorParser import SimulatorParser
 else:
     from SimulatorParser import SimulatorParser
-
 
 # This class defines a complete listener for a parse tree produced by SimulatorParser.
 class SimulatorBaseListener(ParseTreeListener):
@@ -19,6 +17,7 @@ class SimulatorBaseListener(ParseTreeListener):
     def exitInput(self, ctx:SimulatorParser.InputContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#relations.
     def enterRelations(self, ctx:SimulatorParser.RelationsContext):
         pass
@@ -26,6 +25,7 @@ class SimulatorBaseListener(ParseTreeListener):
     # Exit a parse tree produced by SimulatorParser#relations.
     def exitRelations(self, ctx:SimulatorParser.RelationsContext):
         pass
+
 
     # Enter a parse tree produced by SimulatorParser#relation_list.
     def enterRelation_list(self, ctx:SimulatorParser.Relation_listContext):
@@ -35,6 +35,7 @@ class SimulatorBaseListener(ParseTreeListener):
     def exitRelation_list(self, ctx:SimulatorParser.Relation_listContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#relation.
     def enterRelation(self, ctx:SimulatorParser.RelationContext):
         pass
@@ -42,6 +43,7 @@ class SimulatorBaseListener(ParseTreeListener):
     # Exit a parse tree produced by SimulatorParser#relation.
     def exitRelation(self, ctx:SimulatorParser.RelationContext):
         pass
+
 
     # Enter a parse tree produced by SimulatorParser#assignment.
     def enterAssignment(self, ctx:SimulatorParser.AssignmentContext):
@@ -51,6 +53,7 @@ class SimulatorBaseListener(ParseTreeListener):
     def exitAssignment(self, ctx:SimulatorParser.AssignmentContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#determ_relation.
     def enterDeterm_relation(self, ctx:SimulatorParser.Determ_relationContext):
         pass
@@ -58,6 +61,7 @@ class SimulatorBaseListener(ParseTreeListener):
     # Exit a parse tree produced by SimulatorParser#determ_relation.
     def exitDeterm_relation(self, ctx:SimulatorParser.Determ_relationContext):
         pass
+
 
     # Enter a parse tree produced by SimulatorParser#stoch_relation.
     def enterStoch_relation(self, ctx:SimulatorParser.Stoch_relationContext):
@@ -67,6 +71,7 @@ class SimulatorBaseListener(ParseTreeListener):
     def exitStoch_relation(self, ctx:SimulatorParser.Stoch_relationContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#var.
     def enterVar(self, ctx:SimulatorParser.VarContext):
         pass
@@ -74,6 +79,7 @@ class SimulatorBaseListener(ParseTreeListener):
     # Exit a parse tree produced by SimulatorParser#var.
     def exitVar(self, ctx:SimulatorParser.VarContext):
         pass
+
 
     # Enter a parse tree produced by SimulatorParser#range_list.
     def enterRange_list(self, ctx:SimulatorParser.Range_listContext):
@@ -83,6 +89,7 @@ class SimulatorBaseListener(ParseTreeListener):
     def exitRange_list(self, ctx:SimulatorParser.Range_listContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#range_element.
     def enterRange_element(self, ctx:SimulatorParser.Range_elementContext):
         pass
@@ -90,6 +97,7 @@ class SimulatorBaseListener(ParseTreeListener):
     # Exit a parse tree produced by SimulatorParser#range_element.
     def exitRange_element(self, ctx:SimulatorParser.Range_elementContext):
         pass
+
 
     # Enter a parse tree produced by SimulatorParser#constant.
     def enterConstant(self, ctx:SimulatorParser.ConstantContext):
@@ -99,6 +107,7 @@ class SimulatorBaseListener(ParseTreeListener):
     def exitConstant(self, ctx:SimulatorParser.ConstantContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#expression_list.
     def enterExpression_list(self, ctx:SimulatorParser.Expression_listContext):
         pass
@@ -106,6 +115,7 @@ class SimulatorBaseListener(ParseTreeListener):
     # Exit a parse tree produced by SimulatorParser#expression_list.
     def exitExpression_list(self, ctx:SimulatorParser.Expression_listContext):
         pass
+
 
     # Enter a parse tree produced by SimulatorParser#unnamed_expression_list.
     def enterUnnamed_expression_list(self, ctx:SimulatorParser.Unnamed_expression_listContext):
@@ -115,6 +125,7 @@ class SimulatorBaseListener(ParseTreeListener):
     def exitUnnamed_expression_list(self, ctx:SimulatorParser.Unnamed_expression_listContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#mapFunction.
     def enterMapFunction(self, ctx:SimulatorParser.MapFunctionContext):
         pass
@@ -123,45 +134,51 @@ class SimulatorBaseListener(ParseTreeListener):
     def exitMapFunction(self, ctx:SimulatorParser.MapFunctionContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#methodCall.
-    def enter_method_call(self, ctx: SimulatorParser.MethodCallContext):
+    def enterMethodCall(self, ctx:SimulatorParser.MethodCallContext):
         pass
 
     # Exit a parse tree produced by SimulatorParser#methodCall.
-    def exit_method_call(self, ctx: SimulatorParser.MethodCallContext):
+    def exitMethodCall(self, ctx:SimulatorParser.MethodCallContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#objectMethodCall.
-    def enter_object_method_call(self, ctx: SimulatorParser.ObjectMethodCallContext):
+    def enterObjectMethodCall(self, ctx:SimulatorParser.ObjectMethodCallContext):
         pass
 
     # Exit a parse tree produced by SimulatorParser#objectMethodCall.
-    def exit_object_method_call(self, ctx: SimulatorParser.ObjectMethodCallContext):
+    def exitObjectMethodCall(self, ctx:SimulatorParser.ObjectMethodCallContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#distribution.
-    def enter_distribution(self, ctx: SimulatorParser.DistributionContext):
+    def enterDistribution(self, ctx:SimulatorParser.DistributionContext):
         pass
 
     # Exit a parse tree produced by SimulatorParser#distribution.
-    def exit_distribution(self, ctx: SimulatorParser.DistributionContext):
+    def exitDistribution(self, ctx:SimulatorParser.DistributionContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#named_expression.
-    def enter_named_expression(self, ctx: SimulatorParser.Named_expressionContext):
+    def enterNamed_expression(self, ctx:SimulatorParser.Named_expressionContext):
         pass
 
     # Exit a parse tree produced by SimulatorParser#named_expression.
-    def exit_named_expression(self, ctx: SimulatorParser.Named_expressionContext):
+    def exitNamed_expression(self, ctx:SimulatorParser.Named_expressionContext):
         pass
 
+
     # Enter a parse tree produced by SimulatorParser#expression.
-    def enter_expression(self, ctx: SimulatorParser.ExpressionContext):
+    def enterExpression(self, ctx:SimulatorParser.ExpressionContext):
         pass
 
     # Exit a parse tree produced by SimulatorParser#expression.
-    def exit_expression(self, ctx: SimulatorParser.ExpressionContext):
+    def exitExpression(self, ctx:SimulatorParser.ExpressionContext):
         pass
+
 
 
 del SimulatorParser
