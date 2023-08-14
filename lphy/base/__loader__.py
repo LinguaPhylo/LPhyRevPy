d7285895a2d4b8e0713b0bfe09f27649cbee2f66
Walter <walterxie@users.noreply.github.com>
2023-08-15 10:01:42 +1200

    comments

--- lphy/base/__loader__.py
+++ lphy/base/__loader__.py
@@ -6,6 +6,7 @@ import pprint
 import sys
 
 
+# TODO why __loader__ has to be under lphy.base?
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
@@ -19,22 +20,12 @@ def main():
     pp.pprint(found_classes)
 
 
-def get_classes_from_module(module):
-    classes = []
-    for cls_name, obj in inspect.getmembers(module):
-        # exclude parent class
-        if inspect.isclass(obj) and cls_name != 'GenerativeDistribution':
-            classes.append(obj)
-            logging.debug(f"class {cls_name!r} has been imported")
-    return classes
-
-
 def list_classes_in_package(package_name):
     package = import_module(package_name)
     class_list = get_classes_from_module(package)
 
     for loader, module_name, ispkg in pkgutil.walk_packages(package.__path__):
-        #module = loader.find_module(module_name).load_module(module_name)
+        # module = loader.find_module(module_name).load_module(module_name)
         full_module_name = f"{package_name}.{module_name}"
         module = import_module(full_module_name)
         class_list.extend(get_classes_from_module(module))
@@ -42,11 +33,21 @@ def list_classes_in_package(package_name):
     return class_list
 
 
+def get_classes_from_module(module):
+    classes = []
+    for cls_name, obj in inspect.getmembers(module):
+        # TODO better way to exclude parent class?
+        if inspect.isclass(obj) and cls_name != 'GenerativeDistribution':
+            classes.append(obj)
+            logging.debug(f"class {cls_name!r} has been imported")
+    return classes
+
+
 def import_module(module_name):
     if module_name in sys.modules:
         logging.warning(f"{module_name!r} already in sys.modules")
     elif (spec := importlib.util.find_spec(module_name)) is not None:
-        # If you chose to perform the actual import ...
+        # perform the actual import 
         module = importlib.util.module_from_spec(spec)
         sys.modules[module_name] = module
         spec.loader.exec_module(module)
